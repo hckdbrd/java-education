0b506360b81d66ae78c1b314964c7de1d63857d1
hcdkbrd <ennbosh@icloud.com>
2022-07-26 23:58:06 +0300

    base/pattern section is done with 8 of 12 methods.

--- src/main/java/com/knubisoft/base/pattern/PatternTasksImpl.java
+++ src/main/java/com/knubisoft/base/pattern/PatternTasksImpl.java
@@ -1,35 +1,61 @@
 package com.knubisoft.base.pattern;
 
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PatternTasksImpl implements PatternTasks {
 
     @Override
     public boolean haveSetOfCharacters(String text) {
-        return false;
+        if (text == null || text.equals(" ") || text.equals("")) throw new IllegalArgumentException();
+        int textLength = text.length();
+        return text.matches(String.format("\\w{%d}",textLength));
     }
 
     @Override
     public String matchByCharacters(String text) {
-        return null;
+        if (text ==  null) throw new IllegalArgumentException();
+
+        if (text.matches("pq*")) {
+            return "Found a match!";
+        } else {
+            return "Not matched!";
+        }
     }
 
     @Override
     public String matchStartAndEnd(String text) {
-        return null;
+        if (text == null) throw new IllegalArgumentException();
+
+        Pattern pattern = Pattern.compile("\\b[^Gg\\s]\\w*[Gg]\\w*\\w[^Gg\\s]\\b");
+        Matcher matcher = pattern.matcher(text);
+
+        if (matcher.find()) {
+            System.out.println("Found a match!");
+            return "Found a match!";
+        } else {
+            System.out.println("Not matched!");
+            return "Not matched!";
+        }
     }
 
     @Override
     public String matchIpAddress(String text) {
-        return null;
+        if (text == null || text.equals("") || text.equals(" ")) throw new IllegalArgumentException();
+        return text.replaceAll("\\b(0+)(\\d*)\\b","$2");
     }
 
     @Override
     public String matchVowels(String text) {
-        return null;
+        if (text == null || text.equals("") || text.equals(" ")) throw new IllegalArgumentException();
+        return text.replaceAll("(?i)[aeiou]","");
     }
 
     @Override
     public boolean validatePIN(String text) {
-        return false;
+        if (text == null || text.equals("") || text.equals(" ")) throw new IllegalArgumentException();
+        return text.matches("^\\d{4}$|^\\d{6}$|^\\d{8}$");
     }
 
     @Override
@@ -39,12 +65,14 @@ public class PatternTasksImpl implements PatternTasks {
 
     @Override
     public String removeAllNonAlphanumericCharacters(String text) {
-        return null;
+        if (text == null || text.equals("") || text.equals(" ")) throw new IllegalArgumentException();
+        return text.replaceAll("\\W","");
     }
 
     @Override
     public boolean validatePhoneNumber(String text) {
-        return false;
+        if (text == null || text.equals("") || text.equals(" ")) throw new IllegalArgumentException();
+        return text.matches("(\\d{10})|(\\(\\d{3}\\)\\d{3}\\-\\d{4})|(\\(\\d{3}\\)\\d{7})|(\\d{3}\\-\\d{3}\\-\\d{4})");
     }
 
     @Override

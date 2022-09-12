9cab25dfc6627338801fbe976e56c6d272a2bd3a
hcdkbrd <ennbosh@icloud.com>
2022-09-12 23:09:35 +0300

    algorithm/luckyticket task is done. TODO: optimize tests visibility.

--- src/main/java/com/knubisoft/tasks/algorithm/luckyticket/LuckyTicketImpl.java
+++ src/main/java/com/knubisoft/tasks/algorithm/luckyticket/LuckyTicketImpl.java
@@ -1,8 +1,20 @@
 package com.knubisoft.tasks.algorithm.luckyticket;
 
+import java.util.Arrays;
+
 public class LuckyTicketImpl implements LuckyTicket {
     @Override
     public boolean checkIsLuckyTicket(String ticket) {
-        return false;
+        if (ticket == null || !ticket.matches("\\d{6,8}")) return false;
+
+        char[] array = ticket.toCharArray();
+        int lastIndex = array.length-1;
+
+        char[] reversedArray = new char[array.length];
+        for (int i = 0; i <= lastIndex; i++) {
+            reversedArray[i] = array[lastIndex-i];
+        }
+
+        return Arrays.equals(array,reversedArray);
     }
 }
--- src/test/java/com/knubisoft/tasks/algorithm/luckyticket/LuckyTicketTest.java
+++ src/test/java/com/knubisoft/tasks/algorithm/luckyticket/LuckyTicketTest.java
@@ -1,49 +1,21 @@
 package com.knubisoft.tasks.algorithm.luckyticket;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * TODO rewrite tests to JUnit 5
- *
- * parametrized tests
- */
 class LuckyTicketTest {
-
     private final LuckyTicket instance = new LuckyTicketImpl();
-
     /**
      * TODO optimize tests visibility
      */
-    @Test
-    public void notLuckyTicketsTest() {
-        assertFalse(instance.checkIsLuckyTicket(null));
-        assertFalse(instance.checkIsLuckyTicket(""));
-        assertFalse(instance.checkIsLuckyTicket("null"));
-        assertFalse(instance.checkIsLuckyTicket("abCd"));
-        assertFalse(instance.checkIsLuckyTicket("1"));
-        assertFalse(instance.checkIsLuckyTicket("null"));
-        assertFalse(instance.checkIsLuckyTicket("131asd"));
-        assertFalse(instance.checkIsLuckyTicket("123432"));
-        assertFalse(instance.checkIsLuckyTicket("555455"));
-        assertFalse(instance.checkIsLuckyTicket("6666666666"));
-        assertFalse(instance.checkIsLuckyTicket("-555555"));
-        assertFalse(instance.checkIsLuckyTicket("131asd131asd131asd131asd131asd....!@#$%^&*()"));
+    @ParameterizedTest
+    @CsvFileSource(resources = "/tests/luckyticket.csv", numLinesToSkip = 1)
+    public void luckyTicketsTest(String input, String expected) {
+        // Test for null
+        // Test for size
+        // Test for equals
+        assertEquals(Boolean.valueOf(expected), instance.checkIsLuckyTicket(input));
     }
-
-    /**
-     * TODO optimize tests visibility
-     */
-    @Test
-    public void luckyTicketsTest() {
-        assertTrue(instance.checkIsLuckyTicket("123321"));
-        assertTrue(instance.checkIsLuckyTicket("123321"));
-        assertTrue(instance.checkIsLuckyTicket("1234321"));
-        assertTrue(instance.checkIsLuckyTicket("2345432"));
-        assertTrue(instance.checkIsLuckyTicket("23455432"));
-        assertTrue(instance.checkIsLuckyTicket("0000000"));
-    }
-
 }

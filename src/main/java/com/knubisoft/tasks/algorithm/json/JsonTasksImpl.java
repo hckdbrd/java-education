40e015862910a1c1d25fa99b1145f8b28b7d6522
hcdkbrd <ennbosh@icloud.com>
2022-10-03 23:56:29 +0300

    Add: XmlTaskImpl and JsonTaskImpl

--- src/main/java/com/knubisoft/tasks/algorithm/json/JsonTasksImpl.java
+++ src/main/java/com/knubisoft/tasks/algorithm/json/JsonTasksImpl.java
@@ -1,32 +1,77 @@
 package com.knubisoft.tasks.algorithm.json;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.knubisoft.tasks.algorithm.ModelRoot;
+import com.knubisoft.tasks.algorithm.ModelRoot.Item;
 import lombok.SneakyThrows;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class JsonTasksImpl implements JsonTasks {
 
     @Override
     @SneakyThrows
     public ModelRoot parseJson(String json) {
-        return null;
+        if (json == null || json.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        ObjectMapper om = new ObjectMapper();
+        om.findAndRegisterModules();
+        return om.readValue(json, ModelRoot.class);
+
     }
 
+    @SneakyThrows
     @Override
     public List<Integer> getAllIds(String json) {
-        return null;
+        if (json == null || json.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        List<Integer> idList = new ArrayList<>();
+        ObjectMapper om = new ObjectMapper();
+
+        om.findAndRegisterModules();
+
+        ModelRoot modelRoot = om.readValue(json, ModelRoot.class);
+
+        for (Item i : modelRoot.getItems()) {
+            idList.add(i.id);
+        }
+
+        return idList;
     }
 
+    @SneakyThrows
     @Override
     public List<String> getNameWithIdMoreThan1(String json) {
-        return null;
+        if (json == null || json.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        List<String> nameList = new ArrayList<>();
+        ObjectMapper om = new ObjectMapper();
+
+        ModelRoot modelRoot = om.readValue(json, ModelRoot.class);
+
+        for (Item i : modelRoot.getItems()) {
+            if (i.id > 1) {
+                nameList.add(i.name);
+            }
+        }
+
+        return nameList;
     }
 
+    @SneakyThrows
     @Override
-    public List<Map<String,String>> getAllItems(String json) {
-        return null;
+    public List<Map<String, String>> getAllItems(String json) {
+        if (json == null || json.isEmpty()) {
+            throw new NullPointerException();
+        }
+        ObjectMapper om = new ObjectMapper();
+        ModelRoot modelRoot = om.readValue(json, ModelRoot.class);
+        List<Item> itemList = modelRoot.getItems();
+        return itemList.stream().map(item -> Map.of(item.getName(), item.toString())).collect(Collectors.toList());
     }
 
 }
--- src/main/java/com/knubisoft/tasks/algorithm/xml/XMLTasksImpl.java
+++ src/main/java/com/knubisoft/tasks/algorithm/xml/XMLTasksImpl.java
@@ -1,11 +1,14 @@
 package com.knubisoft.tasks.algorithm.xml;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.knubisoft.tasks.algorithm.ModelRoot;
 import lombok.SneakyThrows;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import javax.xml.namespace.QName;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 
@@ -14,17 +17,52 @@ public class XMLTasksImpl implements XMLTasks {
     @Override
     @SneakyThrows
     public ModelRoot parseXML(String xml) {
-        return null;
+        if (xml == null || xml.isEmpty()) {
+            throw new NullPointerException();
+        }
+        XmlMapper mapper = new XmlMapper();
+        mapper.findAndRegisterModules();
+        return mapper.readValue(xml, ModelRoot.class);
     }
 
+    @SneakyThrows
     @Override
     public List<Integer> getAllIds(String xml) {
-        return null;
+        if (xml == null || xml.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        List<Integer> idList = new ArrayList<>();
+        XmlMapper mapper = new XmlMapper();
+
+        mapper.findAndRegisterModules();
+
+        ModelRoot modelRoot = mapper.readValue(xml, ModelRoot.class);
+
+        for (ModelRoot.Item i : modelRoot.getItems()) {
+            idList.add(i.id);
+        }
+
+        return idList;
     }
 
+    @SneakyThrows
     @Override
     public List<String> getNameWithIdMoreThan1(String xml) {
-        return null;
+        if (xml == null || xml.isEmpty()) {
+            throw new NullPointerException();
+        }
+        List<String> nameList = new ArrayList<>();
+        XmlMapper mapper = new XmlMapper();
+
+        ModelRoot modelRoot = mapper.readValue(xml, ModelRoot.class);
+
+        for (ModelRoot.Item i : modelRoot.getItems()) {
+            if (i.id > 1) {
+                nameList.add(i.name);
+            }
+        }
+
+        return nameList;
     }
 
     @SneakyThrows
@@ -32,7 +70,7 @@ public class XMLTasksImpl implements XMLTasks {
         return null;
     }
 
-    private <T> List<T> apply(NodeList nodeList, Function<Node, T> tFunction){
+    private <T> List<T> apply(NodeList nodeList, Function<Node, T> tFunction) {
         return null;
     }
 }
